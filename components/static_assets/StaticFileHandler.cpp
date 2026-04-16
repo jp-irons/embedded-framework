4b242676e2ddd1783f600ffd9662c835ecb685c1
jp-irons <jon@irons.ws>
2026-04-16 10:33:23 +1000

    separated runtime and common assets

--- components/static_assets/StaticFileHandler.cpp
+++ components/static_assets/StaticFileHandler.cpp
@@ -11,40 +11,36 @@ static logger::Logger log{"StaticFileHandler"};
 StaticFileHandler::StaticFileHandler(std::string basePath, std::string defaultFile)
     : base(std::move(basePath))
     , defaultFile(std::move(defaultFile))
-    , table() // EmbeddedAssetTable has no state, so default construct
-{}
+    , table() {
+		log.debug("constructor");
+}
 
 Result StaticFileHandler::handle(http::HttpRequest &request, http::HttpResponse &response) {
     log.debug("handle '%s'", request.path());
-	
-	// TODO review logging - probably over the top.
+
     // Convert string_view → string safely
     std::string resolved = resolvePath(request.uri());
 
-	log.debug("resolved path %s", resolved.c_str());
+    log.debug("resolved path %s", resolved.c_str());
 
     const EmbeddedAsset *asset = table.find(resolved);
     if (!asset) {
         log.warn("Asset not found: %s", resolved.c_str());
-        // TODO fix this       response.setStatus(404);
-		response.sendNotFound404("Asset not found");
-        response.send("Not found");
+        response.sendNotFound404("Asset not found");
         return Result::NotFound;
     }
 
     const char *type = contentTypeForPath(resolved);
     response.setType(type);
     response.send(asset->data, asset->size);
-	return Result::Ok;
+    return Result::Ok;
 }
 
 std::string StaticFileHandler::resolvePath(std::string_view uri) const {
     // Convert string_view → string
     std::string path(uri.data(), uri.size());
-//	log.debug("base='%s' (len=%zu)", base.c_str(), base.size());
-//	log.debug("path='%s'", path.c_str());
 
-    // Must start with base
+	    // Must start with base
     if (!path.starts_with(base)) {
         return ""; // will 404
     }
@@ -52,10 +48,10 @@ std::string StaticFileHandler::resolvePath(std::string_view uri) const {
     // Strip base prefix
     std::string sub = path.substr(base.size());
 
-	// Must start with base
-	if (!sub.starts_with("/")) {
-	    sub = "/" + sub;
-	}
+    // Must start with base
+    if (!sub.starts_with("/")) {
+        sub = "/" + sub;
+    }
 
     // If empty or "/", serve default file
     if (sub.empty() || sub == "/") {
