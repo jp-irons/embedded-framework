453495c0001e82bfca321794b9be07f99d72663a
jp-irons <jon@irons.ws>
2026-04-28 17:51:11 +1000

    migrating towards single embedded server

--- components/embedded_files/files/common_state.js
+++
@@ -1,5 +0,0 @@
-export const state = {
-  ssid: '',
-  password: '',
-  deviceStatus: null
-};
--- components/embedded_files/files/common_validation.js
+++
@@ -1,7 +0,0 @@
-export function validateSSID(ssid) {
-  return ssid && ssid.length > 0;
-}
-
-export function validatePassword(password) {
-  return password.length >= 8;
-}
